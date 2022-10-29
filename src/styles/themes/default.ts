1b7995e0de51d6e79a2f3060242ac1c3de69f1f8
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-10-29 18:41:35 -0300

    feat: add color palette

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+    colors: {
+        white: '#FFFFFF',
+
+        'dark-blue': '#070724',
+        'light-blue': '#419EBB',
+        blue: '#2D68F0',
+
+        'dark-gray': '#38384F',
+        gray: '#838391',
+
+        'light-orange': '#EDA249',
+        'dark-orange': '#D14C32',
+        'orange': '#CD5120',
+
+        'light-red': '#D83A34',
+
+        purple: '#6F2ED6',
+
+        'cyan': '#1EC2A4'
+    },
+} as const
