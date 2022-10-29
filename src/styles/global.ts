df16c8deec86a374e357d55fe09005ac290e54dd
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-10-29 19:02:43 -0300

    feat: add title styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,6 +19,33 @@ export const GlobalStyles = createGlobalStyle`
     body, input, button, textarea {
         font: 400 '0.875rem' 'League Spartan', sans-serif;
     }
+     
+    h1, h2, h3, h4 {
+        color: ${({ theme }) => theme.colors.white};
+    }
+
+    h1 {
+        font: 400 5rem 'Antonio', sans-serif; 
+        line-height: 103px;
+    }
+
+    h2 {
+        font: 400 2.5rem 'Antonio', sans-serif; 
+        line-height: 52px;
+        letter-spacing: -1.5px;
+    }
+
+    h3 {
+        font: 400 0.75rem 'League Spartan', sans-serif; 
+        line-height: 25px;
+        letter-spacing: 2.6px;
+    }
+
+    h4 {
+        font: 400 0.6875rem 'League Spartan', sans-serif; 
+        line-height: 25px;
+        letter-spacing: 1px;
+    }
 
 
 `
