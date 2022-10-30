0b7e2d8d4e65d4c6d9f1968db3d2c7743c655c39
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-10-30 12:30:17 -0300

    feat: decrease margin from Header

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -5,7 +5,7 @@ export const Container = styled('main', {
 })
 
 export const Content = styled('div', {
-    margin: '7.875rem auto 3.5rem',
+    margin: '7rem auto 3.5rem',
 
     maxWidth: '69.375rem',
     width: '100%',
