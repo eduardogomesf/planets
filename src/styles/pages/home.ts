ed2f83f1eedd1b6f2a61465814158b18e6211b3d
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-11-01 10:35:14 -0300

    refactor: change elements in Home styles

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -1,10 +1,10 @@
 import { styled } from "..";
 
-export const Container = styled('main', {
+export const Container = styled('div', {
     width: '100%'
 })
 
-export const Content = styled('div', {
+export const Content = styled('main', {
     margin: '7rem auto 2rem',
 
     maxWidth: '69.375rem',
